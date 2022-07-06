a4280760228f697a0b42921ce66ac07f5651661d
nrajulearning <nrajulearning@gmail.com>
2022-07-06 10:56:56 +0530

    updated network.tf with subnet and internet gateway resources

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -6,3 +6,31 @@ resource "aws_vpc" "mylab-vpc" {
       "Name" = "MyVPC-test"
     }  
 }
+
+# Create Public subnet
+
+resource "aws_subnet" "mylab-subnet1-public" {
+    vpc_id = aws_vpc.mylab-vpc.id
+    cidr_block = "172.20.10.0/24"
+    tags = {
+      "Name" = "Subnet1-public"
+    }
+}
+
+# Create Private Subnet
+resource "aws_subnet" "mylab-subnet2-private" {
+    vpc_id = aws_vpc.mylab-vpc.id
+    cidr_block = "172.20.30.0/24"
+    tags = {
+      "Name" = "Subnet2-private"
+    } 
+}
+
+# Enable Internet Gateway for VPC Network
+
+resource "aws_internet_gateway" "mylab-IntGW" {
+    vpc_id = aws_vpc.mylab-vpc.id
+    tags = {
+      "Name" = "MyVPC-IGW"
+    } 
+}
